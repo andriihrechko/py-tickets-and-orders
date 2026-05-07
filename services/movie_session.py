2a2a57dd2c3cc152ba436a66776ff532f5c2b2e3
andriihrechko <andriihrechko250703@gmail.com>
2026-05-08 02:04:39 +0300

    style: add blank lines and annotations

--- services/movie_session.py
+++ services/movie_session.py
@@ -44,7 +44,7 @@ def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
 
 
-def get_taken_seats(movie_session_id):
+def get_taken_seats(movie_session_id: int) -> list[Ticket]:
     return list(
         Ticket.objects.filter(
             order__tickets__movie_session_id=movie_session_id
