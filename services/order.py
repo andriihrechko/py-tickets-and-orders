b715d6312fe38adaf55eb7760b03b8318e7a61fb
andriihrechko <andriihrechko250703@gmail.com>
2026-05-08 02:14:05 +0300

    some refactor: fix some blanks

--- services/order.py
+++ services/order.py
@@ -11,7 +11,7 @@ from db.models import Order, Ticket
 def create_order(
         tickets: list[dict],
         username: str,
-        date: datetime
+        date: datetime = None
 ) -> Order:
     user = get_user_model().objects.get(username=username)
     order = Order.objects.create(
