71823101d8dde40162683ce9de76f316a7c74b49
andriihrechko <andriihrechko250703@gmail.com>
2026-05-08 23:59:51 +0300

    refactor classes and annotations

--- services/user.py
+++ services/user.py
@@ -10,17 +10,20 @@ def create_user(
         first_name: str = None,
         last_name: str = None
 ) -> User:
+    if not email:
+        email = ""
+    if not first_name:
+        first_name = ""
+    if not last_name:
+        last_name = ""
+
     user = get_user_model().objects.create_user(
         username=username,
         password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name
     )
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    user.save()
     return user
 
 
